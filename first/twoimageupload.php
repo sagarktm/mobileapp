cfbe8329c3fa2c269d13945dfa85bcaee4fb8d72
suresh chand <suresh.chand@syntechnepal.com>
2012-09-07 17:46:06 +0545

    three files added

---
+++ first/twoimageupload.php
@@ -0,0 +1,55 @@
+"<html>
+
+<head>
+
+<body>
+
+<form action="<?php print $PHP_SELF?>" enctype="multipart/form-data" method="post">
+
+Upload the following files:<br />
+
+<input name="userfile[]" type="file" /><br />
+
+<input name="userfile[]" type="file" /><br />
+
+<input type="submit" value="Submit Files" />
+
+</form>"
+
+<?php
+
+$numberOfFiles = $_POST['numberOfFiles'];
+
+for($i = 0; $i < $numberOfFiles; $i++){
+
+$file_name = "images/".$_FILES['uploadedFiles'. $i]['name'];
+
+$file_name = "images/".stripslashes($file_name);
+
+$file_name = str_replace("'","",$file_name);
+
+$copy = copy($_FILES['uploadFile'. $i]['tmp_name'],"images/".$file_name);
+
+// prompt if successfully copied
+
+if($copy){
+
+echo "$file_name | uploaded sucessfully!";
+
+}else{
+
+echo "$file_name | could not be uploaded!";
+
+}
+
+}
+
+?>
+
+</body>
+
+</head>
+
+</html>"
+
+
